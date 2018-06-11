5d0d924472e228694b46a6233c8c541d4b826006
Jon Farrell <jon@jonfarrell.io>
2018-06-10 20:51:58 -0400

    Semantic HTML fixes for better accessibility

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -209,7 +209,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
@@ -256,6 +256,7 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
+  li.setAttribute('aria-current','page')
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 }
