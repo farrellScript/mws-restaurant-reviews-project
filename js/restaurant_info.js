6d4a9e97bb25c0663879303002330c48bc03c4d8
Jon Farrell <jon@jonfarrell.io>
2018-06-16 14:23:11 -0400

    Project update to use leaflet/mapbox instead of Google Maps

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,5 +1,5 @@
 let restaurant;
-var map;
+var newMap;
 
 /**
  * Check to see if service worker is supported by the browser 
@@ -114,23 +114,41 @@ if ('serviceWorker' in navigator) {
 
 
 /**
- * Initialize Google map, called from HTML.
+  * Initialize map as soon as the page is loaded.
  */
-window.initMap = () => {
+document.addEventListener('DOMContentLoaded', (event) => {  
+  initMap();
+  });
+  
+  /**
+   * Initialize leaflet map
+   */
+  initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
+    } else {      
+      self.newMap = L.map('map', {
+        center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
+        scrollWheelZoom: false
       });
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: '<your MAPBOX API KEY HERE>',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'    
+      }).addTo(newMap);
       fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
-}
+  }  
+  
+  /* window.initMap = () => {
+  } */
 
 /**
  * Get current restaurant from page URL.
